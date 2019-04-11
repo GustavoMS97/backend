3c2947bb727cd694626e39c40b62528c540297cb
Gustavo Martins do Santos <gustavoms97@hotmail.com>
2019-04-11 15:07:08 -0300

    Estrutura backend

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multer = require('multer');
+
+const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
+
+const multerConfig = require('./config/multer');
+
+const routes = express.Router();
+
+routes.post('/boxes', BoxController.store);
+routes.get('/boxes/:id', BoxController.show);
+routes.post(
+  '/boxes/:id/files',
+  multer(multerConfig).single('file'),
+  FileController.store,
+);
+
+module.exports = routes;
